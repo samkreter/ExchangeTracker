445dd1ef65ab5f6a57215f194304edfac20c1540
Sam Kreter <samkreter@gmail.com>
2015-07-26 06:33:36 -0400

    added filter still has small bugs

--- app/controllers/countries.js
+++ app/controllers/countries.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    filterText: '',
+    filteredResults: function() {
+        var filter = this.get('filterText');
+        return this.get('model').filter(function(item) {
+          return item.get('name').toLowerCase().indexOf(filter.toLowerCase()) !== -1;
+        });
+    }.property('filterText')
 });
