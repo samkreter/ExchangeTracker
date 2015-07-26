5f574231aa360a3b5f1f632f43a8ff938245afa4
Sam Kreter <samkreter@gmail.com>
2015-07-26 00:07:56 -0400

    added function to computed data for the chart

--- app/controllers/countries.js
+++ app/controllers/countries.js
@@ -1,41 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    company: Ember.computed.alias("controllers.company"),
-
-    countryNames: function(){
-        var labels = [];
-        this.get('model').forEach(function(country) {
-             labels.push(country.get('name'));
-        });
-        debugger;
-    }.property('model'),
-
-    chartData: function(){
-        return {
-            labels: this.get("countryNames"),
-            datasets: [
-                {
-                    label: this.get("company").get("name"),
-                    fillColor: "rgba(220,220,220,0.2)",
-                    strokeColor: "rgba(220,220,220,1)",
-                    pointColor: "rgba(220,220,220,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: [65, 59, 80, 81, 56, 55, 40]
-                },
-                {
-                    label: "My Second dataset",
-                    fillColor: "rgba(151,187,205,0.2)",
-                    strokeColor: "rgba(151,187,205,1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(151,187,205,1)",
-                    data: [28, 48, 40, 19, 86, 27, 90]
-                }
-            ]
-        };
-    }.property('company')
 });
--- app/controllers/country.js
+++ app/controllers/country.js
@@ -1,4 +1,50 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    needs: ['Countries'],
+    CountriesModel: Ember.computed.alias('controllers.Countries.model'),
+
+    countryDates: function(){
+        var labels = [];
+        this.get('countryDays').then((function(data){
+            data.forEach(function(day){
+                labels.push(day.get('date'));
+            });
+        }));
+        return labels;
+    }.property('model'),
+
+    countryDays: function(){
+        return this.store.find('day', {country_code: this.get('model').get('country_code')});
+    }.property('model'),
+
+    countryData: function(){
+        var dataForChart = [];
+        this.get('countryDays').then((function(data){
+            data.forEach(function(day){
+                dataForChart.push(day.get('rate'));
+            });
+        }));
+        return dataForChart;
+    }.property('model'),
+
+    chartData: function(){
+        var test = {
+            labels: this.get("countryDates"),
+            datasets: [
+                {
+                    label: this.get("model").get("name"),
+                    fillColor: "rgba(220,220,220,0.2)",
+                    strokeColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: this.get('countryData')
+                }
+            ]
+        };
+        debugger;
+        return test;
+    }.property('model')
 });
