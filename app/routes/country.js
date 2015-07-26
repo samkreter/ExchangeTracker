16df4b9856684ba2a9eba0d44e4e2650fbabc2b3
Sam Kreter <samkreter@gmail.com>
2015-07-26 06:13:06 -0400

    removed console.log

--- app/routes/country.js
+++ app/routes/country.js
@@ -9,12 +9,13 @@ export default Ember.Route.extend({
         return this.store.find('day', {country_code: model.get('country_code')}).then(function(data){
             var labels = [];
             var dataForChart = [];
+            var count = 0;
             data.forEach(function(day){
                 labels.push(day.get('date'));
                 dataForChart.push(day.get('rate'));
+                count += 1;
+                if (count > 4){return true;}
             });
-            console.log(labels);
-            console.log(dataForChart);
             _this.set("labels",labels);
             _this.set("dataForChart",dataForChart);
         });
